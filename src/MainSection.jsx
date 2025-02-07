91279148c06e0c6a96710990914d6e82d6e73a5f
Asal Yousefi <asalyousefi8866@gmail.com>
2025-02-07 16:03:34 +0330

    default bg

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -6,9 +6,23 @@ export default function MainSection(props) {
     <main>
       <div className="container d-flex justify-content-center mt-3">
         {props.tasks.length === 0 ? (
-          <img src="images/Detective-check-footprint 1.svg" alt="default-bg" />
+          <div>
+            <img
+              src="images/Detective-check-footprint 1 (1).svg"
+              alt="default-bg"
+            />
+            <p className="text-center fs-4" style={{fontWeight: 600}}>Empty...</p>
+          </div>
         ) : (
-          <Task setEditMode={props.setEditMode} editMode={props.editMode} tasks={props.tasks} setShowPopup={props.setShowPopup} setTasks={props.setTasks} setTaskInput={props.setTaskInput} taskInput={props.taskInput} />
+          <Task
+            setEditMode={props.setEditMode}
+            editMode={props.editMode}
+            tasks={props.tasks}
+            setShowPopup={props.setShowPopup}
+            setTasks={props.setTasks}
+            setTaskInput={props.setTaskInput}
+            taskInput={props.taskInput}
+          />
         )}
       </div>
       <PopupButton setShowPopup={props.setShowPopup} />
